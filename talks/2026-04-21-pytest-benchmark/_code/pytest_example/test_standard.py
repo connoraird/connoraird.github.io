7f59e6348fa291f85dd098b888d4094791dc209a
connoraird <c.aird@ucl.ac.uk>
2026-04-14 15:40:15 +0100

    Rename pytest benchmark talk

--- talks/2026-04-21-pytest-benchmark/_code/pytest_example/test_standard.py
+++ talks/2026-04-21-pytest-benchmark/_code/pytest_example/test_standard.py
@@ -17,6 +17,6 @@ def inputs():
 def outputs():
     return [2,3,4,5,6,7,8,9,10,11]
 
-def test_inc(inputs, outputs):
+def test_inc_with_fixtures(inputs, outputs):
     for i in range(len(inputs)):
       assert inc(inputs[i]) == outputs[i]
